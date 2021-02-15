43849e73637946cba8fd8141cb49c74cd60e821e
yeonsookim-wt <75154118+yeonsookim-wt@users.noreply.github.com>
2021-02-15 21:45:25 +0900

    Add files via upload

---
+++ tests/test_core.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class LgtmTest(unittest.TestCase):
+    def test_lgtm(self):
+        from lgtm.core import lgtm
+        self.assertIsNone(lgtm())
+
+    def test_lgtm(self):
+        from lgtm.core import lgtm
+        self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))
